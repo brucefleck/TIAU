0a5eea72835e10f6d416e7ba1ea2a9d661cfa1e4
Bruce <brucematiasfleck@gmail.com>
2023-11-19 21:45:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -9,6 +9,17 @@ def index() :
 @app.route('/registro.html')
 def registro():
     return render_template('registro.html')
+
+@app.route('/process_login', methods=['POST'])
+def process_login():
+    correo = request.form['correo']
+    contrasenia = request.form['contrasenia']
+
+    return redirect(url_for('homepage'))
+
+@app.route('/homepage.html')
+def homepage():
+    return render_template('homepage.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
